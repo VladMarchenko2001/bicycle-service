46ae63048cc7b83baec61224f2fe364010961b6a
Marchenko <vladon.marchenko2001@gmail.com>
2022-01-20 18:24:31 +0200

    fixed typo

--- src/main/java/bicycle/web/filter/AuthenticationFilter.java
+++ src/main/java/bicycle/web/filter/AuthenticationFilter.java
@@ -31,7 +31,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpSession session = request.getSession();
-        Long driverId = (Long) session.getAttribute("user_id");
+        Long driverId = (Long) session.getAttribute(USER_ID);
         if (driverId == null && !allowedUrls.contains(request.getServletPath())) {
             response.sendRedirect("/login");
             return;
